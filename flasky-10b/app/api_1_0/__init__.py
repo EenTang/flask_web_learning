0b3b62c72d1e70d85828f6b53fd87ba71726e014
Een Tang <dengbulieduo@163.com>
2016-06-14 15:33:38 +0800

    add flasky-10b 14a

---
+++ flasky-10b/app/api_1_0/__init__.py
@@ -0,0 +1,6 @@
+#APP blueprint constructor
+from flask import Blueprint
+
+api = Blueprint('api', __name__)
+
+from . import authentication, posts, users, comments, errors
---
+++ flasky-10b/app/api_1_0/errors.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*- 
+from flask import jsonify
+from app.exceptions import ValidationError
+from . import api
+
+
+
+#API error handler for status code 400
+def bad_request(message):
+	response = jsonify({'error': 'bad request', 'message': message})
+	response.status_code = 400
+	return response
+
+#API error handler for status code 401
+def unauthorized(message):
+	response = jsonify({'error': 'unauthorized', 'message': message})
+	response.status_code = 401
+	return response	
+
+#API error handler for status code 403
+def forbidden(message):
+	response = jsonify({'error': 'forbidden', 'message': message})
+	response.status_code = 403
+	return response
+
+#API中ValidationError异常的处理程序，这个修饰器从API蓝本中调用，所以只有当处理蓝本中的路由时抛出了异常才会调用这个处理程序。
+@api.errorhandler(ValidationError)
+def validation_error(e):
+	return bad_request(e.args[0])
