56ad02c3a77cc173ce7a8b6d7099f0f72e06ad73
Een Tang <dengbulieduo@163.com>
2016-06-16 20:44:38 +0800

    add flasky-15d2

--- flasky-10b/tests/test_selenium.py
+++ flasky-10b/tests/test_selenium.py
@@ -1,96 +1,100 @@
+from selenium import webdriver
 import re
+import unittest
+from app import create_app, db
+from flask import url_for
+from app.models import User, Role, Post
 import threading
 import time
-import unittest
-from selenium import webdriver
-from app import create_app, db
-from app.models import Role, User, Post
 
 
+
+# shows the layout of a test case that is configured to run tests with Selenium
 class SeleniumTestCase(unittest.TestCase):
-    client = None
-    
-    @classmethod
-    def setUpClass(cls):
-        # start Firefox
-        try:
-            cls.client = webdriver.Ie()
-        except:
-            pass
+	client = None
 
-        # skip these tests if the browser could not be started
-        if cls.client:
-            # create the application
-            cls.app = create_app('testing')
-            cls.app_context = cls.app.app_context()
-            cls.app_context.push()
+	@classmethod
+	def setUpClass(cls):
+		# start Ie
+		try:
+			cls.clent = webdriver.Ie()
+		except:
+			pass
 
-            # suppress logging to keep unittest output clean
-            import logging
-            logger = logging.getLogger('werkzeug')
-            logger.setLevel("ERROR")
+		# skip these tests if the browser could not be started
+		if cls.client:
+			# create the application
+			cls.app = create_app('testing')
+			cls.app_context = cls.app.app_context()
+			cls.app_context.push()
 
-            # create the database and populate with some fake data
-            db.create_all()
-            Role.insert_roles()
-            User.generate_fake(10)
-            Post.generate_fake(10)
+			# suppress logging to keep unittest output clean
+			import logging
+			logger = logging.getLogger('werkzeug')
+			logger.setLevel("ERROR")
 
-            # add an administrator user
-            admin_role = Role.query.filter_by(permissions=0xff).first()
-            admin = User(email='john@example.com',
-                         username='john', password='cat',
-                         role=admin_role, confirmed=True)
-            db.session.add(admin)
-            db.session.commit()
+			# create the database and populate with some fake database
+			db.create_all()
+			Role.insert_roles()
+			User.generate_fake(10)
+			Post.generate_fake(10)
 
-            # start the Flask server in a thread
-            threading.Thread(target=cls.app.run).start()
+			# add an administrator User
+			admin_role = Role.query.filter_by(permission=0xff).first()
+			admin = User(email='john@example.com', 
+						username='john', password='cat',
+						role=admin_role, confirmed=True)
+			db.session.add(admin)
+			db.session.commit()
 
-            # give the server a second to ensure it is up
-            time.sleep(1) 
+			# start the Flask server in a thread
+			threading.Tread(target=cls.app.run).start()
 
-    @classmethod
-    def tearDownClass(cls):
-        if cls.client:
-            # stop the flask server and the browser
-            cls.client.get('http://localhost:5000/shutdown')
-            cls.client.close()
+			# give the server a second to ensure it is up
+			time.sleep(1)
 
-            # destroy database
-            db.drop_all()
-            db.session.remove()
 
-            # remove application context
-            cls.app_context.pop()
+	@classmethod
+	def tearDownClass(cls):
+		if cls.client:
+			# stop the flask server and the browser
+			cls.client.get('http://localhost:5000/shutdown')
+			cls.client.close()
 
-    def setUp(self):
-        if not self.client:
-            self.skipTest('Web browser not available')
+			# destroy database
+			db.drop_all()
+			db.session.remove()
 
-    def tearDown(self):
-        pass
-    
-    def test_admin_home_page(self):
-        # navigate to home page
-        self.client.get('http://localhost:5000/')
-        self.assertTrue(re.search('Hello,\s+Stranger!',
-                                  self.client.page_source))
+			# remove application context 
+			cls.app_context.pop()
 
-        # navigate to login page
-        self.client.find_element_by_link_text('Log In').click()
-        self.assertTrue('<h1>Login</h1>' in self.client.page_source)
+	def setUp(self):
+		if not self.client:
+			self.skipTest('Web browser not available')
 
-        # login
-        self.client.find_element_by_name('email').\
-            send_keys('john@example.com')
-        self.client.find_element_by_name('password').send_keys('cat')
-        self.client.find_element_by_name('submit').click()
-        self.assertTrue(re.search('Hello,\s+john!', self.client.page_source))
+	def tearDown(self):
+		pass
 
-        # navigate to the user's profile page
-        self.client.find_element_by_link_text('Profile').click()
-        self.assertTrue('<h1>john</h1>' in self.client.page_source)
+	def test_admin_home_page(self):
+		# navigate to home page
+		self.client.get('http://localhost:5000/')
+		self.assertTrue(re.search('Hello,\s+Stranger!',
+									self.client.page_source))
+
+		# navigate to login page
+		self.client.find_element_by_link_text('Log In').click()
+		self.assertTrue('<h1>Login</h1>' in self.client.page_source)
+
+		# login
+		self.client.find_element_by_name('email').\
+			send_keys('john@example.com')
+		self.client.find_element_by_name('password').send_keys('cat')
+		self.client.find_element_by_name('submit').click()
+		self.assertTrue(re.search('Hello,\s+john!', self.client.page_source))
+
+		# navigate to the user's profile page
+		self.client.find_element_by_link_text('Profile').click()
+		self.assertTrue('<h1>john</h1>' in self.client.page_source)
 
 
 
